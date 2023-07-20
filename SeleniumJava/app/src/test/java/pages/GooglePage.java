64a3c8812ea05f82132b69a5e9c8846ebb0f3969
cramirez85 <malmsteen.bisom@gmail.com>
2023-07-20 17:54:55 -0400

    todo el code

---
+++ SeleniumJava/app/src/test/java/pages/GooglePage.java
@@ -0,0 +1,24 @@
+package pages;
+
+public class GooglePage extends BasePage {
+
+    private String searchButton = "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[5]/center/input[1]";
+    private String searchTextField = "//textarea[@id='APjFqb']";
+
+    public GooglePage() {
+        super(driver);
+    }
+
+    public void navigateToGoogle() {
+        navigateTo("https://www.google.cl");
+    }
+
+    public void clickGoogleSearch() {
+        clickElement(searchButton);
+    }
+
+    public void enterSearchCriteria(String criteria) {
+        write(searchTextField, criteria);
+    }
+
+}
